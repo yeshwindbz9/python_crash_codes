70ba28f3afa84f09d09f7cab0f784906b68941d5
yeshwin <64612584+yeshwindbz9@users.noreply.github.com>
2021-06-15 16:51:01 +0530

    Create numsVars.py

---
+++ numsVars.py
@@ -0,0 +1,13 @@
+print('Numbers and Variables')
+#numbers
+print("29.0 + 5: ", 29.0+5)
+print("3 ^ 3: ", 3**3)
+#order of operations
+print("2 + 10 * 10 + 3: ", 2+10*10+3)
+print("(2 + 10) * (10 + 3): ", (2+10)*(10+3))
+#variables
+a = 10
+print("a++ : ", a+a)
+b = 5
+c = 4
+print("b ^ 2 - 4 * a * c: ", b^2 - 4*a*c)
