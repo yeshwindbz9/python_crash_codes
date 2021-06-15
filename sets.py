6c0f3a0fb6dc2c1b69966ead13a790d4cfc22401
yeshwin <64612584+yeshwindbz9@users.noreply.github.com>
2021-06-15 16:51:19 +0530

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,13 @@
+#sets
+x = set()
+#collections of unique elements
+print(x)
+x.add(1)
+print(x)
+x.add(2)
+x.add(3)
+x.add(3)
+print(x)
+#getting uniques values from a list
+l = [1,1,2,2,3,3,4,5,6,7,7,7,7,7,8,9]
+print(set(l))
