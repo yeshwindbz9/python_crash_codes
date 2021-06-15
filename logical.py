10f2010c481a42f8eabf65741257e40b3bb4d520
yeshwin <64612584+yeshwindbz9@users.noreply.github.com>
2021-06-15 16:50:22 +0530

    Create logical.py

---
+++ logical.py
@@ -0,0 +1,12 @@
+# comparision operators: <, >, ==, !=, <=, >=
+print("1>2: ",1>2)
+print("1<2: ", 1<2)
+print("1==2: ", 1==2)
+print("1==1: ", 1==1)
+print("1!=2: ", 1!=2)
+print("3>=2: ", 3>=2)
+print("3<=2: ", 3<=2)
+# logical operators: and, or, not
+print("1==1 and 2<3: ", 1==1 and 2>3)
+print("1==1 and 2<3: ", 1==1 or 2<3)
+print("1 not in [1, 2, 3]: ", 1 not in [1, 2, 3])
