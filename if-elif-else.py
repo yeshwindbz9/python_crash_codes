874abae20980e344479b34070ed61d101786bfef
yeshwin <64612584+yeshwindbz9@users.noreply.github.com>
2021-06-15 16:39:55 +0530

    Create if-elif-else.py

---
+++ if-elif-else.py
@@ -0,0 +1,11 @@
+mks = 50
+if(mks<30):
+    print("Grade D")
+elif(mks>=30 and mks<50):
+    print("Grade C")
+elif(mks>=50 and mks<70):
+    print("Grade B")
+elif (mks>=70 and mks<=100):
+    print("Grade A")
+else:
+    print("Enter valid marks!")
